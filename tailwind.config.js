b1ff06f3a1d6cd3d4bd37da32d07c6858606babe
Aaron Styles <aaron@charcoalstyles.com>
2022-04-06 14:30:42 +1000

    added call to API

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@ module.exports = {
   content: [
     "./src/**/*.{html,js}",
   ],
+  safelist: [
+    'border-red-700',
+    'border-orange-700',
+    'border-yellow-700',
+    'border-green-700',
+    'border-blue-700',
+    'border-indigo-700',
+    'border-purple-700',
+    'border-emerald-700',
+    'border-amber-700',
+    'border-fuchsia-700',
+  ],
   theme: {
     extend: {},
   },
